2d0f143b9944ffb7a2e724759f83d14c8308a228
Datim <jroecks@gmail.com>
2016-04-06 22:21:21 -0700

    Update git prompt script

--- unix/setup.sh
+++ unix/setup.sh
@@ -10,7 +10,10 @@ wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-pro
 
 # add commands to end of bash file
 GIT_PROMPT="PS1='[\u@\h \W\$(__git_ps1 \" (%s)\")]\$ '" 
+BASH_FILE=~/.bashrc
 
-echo "source ${SCRIPT_PATH}/git-prompt.sh" >> ~/.bashrc
-echo ${GIT_PROMPT} >> ~/.bashrc
+echo "" >> ${BASH_FILE}
+echo "#For command line prompt display of a git branch" >> ${BASH_FILE}
+echo "source ${SCRIPT_PATH}/git-prompt.sh" >> ${BASH_FILE}
+echo ${GIT_PROMPT} >> ${BASH_FILE}
 
