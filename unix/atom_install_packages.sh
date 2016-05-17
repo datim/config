734dfcd5aa4c69e33c360884848f5bcc32692440
jeffrey-rus-roecks <jeffrey-rus-roecks@github.hpe.com>
2016-05-17 14:00:13 -0700

    Add atom config file

---
+++ unix/atom_install_packages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Install atom packages
+# Mostly from: https://www.sitepoint.com/10-essential-atom-add-ons/
+apm install minimap
+apm install highlight-selected
+apm install minimap-highlight-selected
+apm install autoclose-html
+apm install open-recent
+
+# lint
+apm install linter
+apm install linter-shellcheck
+apm install linter-haml
+apm install linter-javac
+
+# js lint
+apm install linter-jshint
+apm install linter-jscs
+apm install linter-flow
+
+# python lint
+apm install linter-pylint
+apm install linter-pep8
+
